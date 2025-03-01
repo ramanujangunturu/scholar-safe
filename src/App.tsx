703505f4363015f982a3122ba714e36fb7d94c55
ramanujan gunturu <ramanujangunturu@gmail.com>
2025-03-01 12:56:20 +0530

    frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import { SpotlightPreview } from './components/elements/Background'
+import { ShootingStarsAndStarsBackgroundDemo } from './components/elements/Background2'
+import { BackgroundBeamsDemo } from './components/elements/BeamsBackground'
+function App() {
+
+  return (
+    <>
+      <BackgroundBeamsDemo />
+    </>
+  )
+}
+
+export default App
